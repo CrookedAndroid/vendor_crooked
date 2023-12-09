d3ad5890bf269f5e568681a8f4eac62696e5041f
hurtcopain <jacksummers715@gmail.com>
2023-12-08 20:28:20 -0500

    Import CarrierSettings and Overlays
    Change-Id: I31091b6fa5013a263c71a0388d66eb9db002be89

--- config/common.mk
+++ config/common.mk
@@ -118,3 +118,8 @@ endif
 # Google Faceunlock
 PRODUCT_PACKAGES += \
     PixelTrafficLightFaceOverlay
+
+# Inherit from rro_overlays config
+$(call inherit-product, vendor/crooked/config/rro_overlays.mk)
+# Inherit from rro_overlays config
+$(call inherit-product, vendor/crooked/config/telephony.mk)
